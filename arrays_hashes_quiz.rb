5cdcb486111353f4cc71ac11be3f81c5e47e1ed0
Edward Fallon <eddie_fallon@hotmail.com>
2018-03-28 16:16:12 +0100

    deleted Livingston

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -10,18 +10,19 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 
 # 1. Add `"Edinburgh Waverley"` to the end of the array
 stops.push("Edinburgh Waverley")
-p stops
+
 
 # 2. Add `"Glasgow Queen St"` to the start of the array
 stops.unshift("Glasgow Queen St")
-p stops
+
 # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
 stops.insert(4, "Polmont")
-p stops
 # 4. Work out the index position of `"Linlithgow"`
 look_for_position = stops.find_index("Linlithgow")
-p look_for_position
+
 # 5. Remove `"Livingston"` from the array using its name
+stops.delete("Livingston")
+p stops
 # 6. Delete `"Cumbernauld"` from the array by index
 # 7. How many stops there are in the array?
 # 8. How many ways can we return `"Falkirk High"` from the array?
